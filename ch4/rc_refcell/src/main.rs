c14d4b60e3cf353a378c9a5e8627459590b7a003
jtfeng <jiangtao.feng@baoying.com>
2023-04-09 23:17:24 +0800

    f

--- ch4/rc_refcell/src/main.rs
+++ ch4/rc_refcell/src/main.rs
@@ -1,4 +1,21 @@
-fn main() {
-  println!("Hello, world!");
+use std::{cell::RefCell, rc::Rc};
+#[derive(Debug)]
+struct GroundStation {
+  radio_freq: f64,
 }
 
+fn main() {
+  let base: Rc<RefCell<GroundStation>> = Rc::new(RefCell::new(GroundStation { radio_freq:80.0 }));
+
+  println!("base {:?}", base);
+  {
+    let mut base_2 = base.borrow_mut();
+    base_2.radio_freq -= 10.0;
+    println!("base_2: {:?}", base_2);
+  }
+  println!("base:{:?}", base);
+  let mut base_3 = base.borrow_mut();
+  base_3.radio_freq += 40.0;
+  println!("base {:?}", base);
+  println!("base_3: {:?}", base_3);
+}
