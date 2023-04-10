36944c2b40b79723decabd5cfc26a92536b5b40c
jtfeng <jiangtao.feng@baoying.com>
2023-04-10 10:50:45 +0800

    f

---
+++ ch5/impossible_add/src/main.rs
@@ -0,0 +1,6 @@
+#[allow(arithmetic_overflow)]
+fn main() {
+  let (a, b) = (200, 100);
+  let c: u8 = a + b; // attempt to compute `200_u8 + 100_u8`, which would overflow
+  println!("200 + 100 = {}", c); // 因为抛出 panic，无法执行完毕。
+}
