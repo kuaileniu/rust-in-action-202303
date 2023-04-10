78c46df65ab36da9f4849142b0b784fe3ae37c8b
jtfeng <jiangtao.feng@baoying.com>
2023-04-10 10:54:31 +0800

    f

--- ch5/impossible_add/src/main.rs
+++ ch5/impossible_add/src/main.rs
@@ -3,4 +3,6 @@ fn main() {
   let (a, b) = (200, 100);
   let c: u8 = a + b; // attempt to compute `200_u8 + 100_u8`, which would overflow
   println!("200 + 100 = {}", c); // 因为抛出 panic，无法执行完毕。
+  // 若定要编译执行成功需：
+  // rustc -O main.rs  && ./main
 }
